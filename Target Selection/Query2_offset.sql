72aeb1df6e829c82f5c5d606cb15def3826236c0
Kanyuni Iemoto <iemotoyuni@gmail.com>
2025-08-14 14:31:45 +0900

    Update Query2_offset.sql

--- Target Selection/Query2_offset.sql
+++ Target Selection/Query2_offset.sql
@@ -3,7 +3,10 @@ SELECT
     f.r_mag_offset,
     f.i_mag_offset,
     f.z_mag_offset,
-    f.y_mag_offset
+    f.y_mag_offset,
+    (f.skymap_id / 10000)          AS tract,
+    ((f.skymap_id % 10000) / 100)  AS patch_x,
+    (f.skymap_id % 100)            AS patch_y
 FROM pdr3_dud_rev.stellar_sequence_offsets AS f
 WHERE
       (f.skymap_id / 10000) BETWEEN 10054 AND 10056
