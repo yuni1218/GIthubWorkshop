6f05b73f6ae5d593b9e547e3946ed086536e4cf2
Kanyuni Iemoto <iemotoyuni@gmail.com>
2025-08-14 14:29:17 +0900

    Update Query2_offset.sql

--- Target Selection/Query2_offset.sql
+++ Target Selection/Query2_offset.sql
@@ -1,7 +1,11 @@
 SELECT
-  f.g_mag_offset,
-  f.r_mag_offset,
-  f.i_mag_offset,
-  f.z_mag_offset,
-  f.y_mag_offset
+    f.g_mag_offset,
+    f.r_mag_offset,
+    f.i_mag_offset,
+    f.z_mag_offset,
+    f.y_mag_offset
 FROM pdr3_dud_rev.stellar_sequence_offsets AS f
+WHERE
+      (f.skymap_id / 10000) BETWEEN 10054 AND 10056
+   OR (f.skymap_id / 10000) BETWEEN 9812  AND 9814
+   OR (f.skymap_id / 10000) BETWEEN 9572  AND 9869;
